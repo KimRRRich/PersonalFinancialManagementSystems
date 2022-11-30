ce2d432db4d0957f8924946deb8c6523a3866349
zx <102405740+KimRRRich@users.noreply.github.com>
2022-11-30 16:21:46 +0800

    add mianpage

--- app/src/main/java/edu/hdu/personalfinancialmanagementsystems/Login.java
+++ app/src/main/java/edu/hdu/personalfinancialmanagementsystems/Login.java
@@ -3,12 +3,29 @@ package edu.hdu.personalfinancialmanagementsystems;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    EditText Account,Password;
+    Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Account=findViewById(R.id.Login_Account);
+        Password=findViewById(R.id.Login_Password);
+        Login=findViewById(R.id.Login_Login);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String accountString,passwordString;
+                accountString=Account.getText().toString();
+                passwordString=Password.getText().toString();
+                if()
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/edu/hdu/personalfinancialmanagementsystems/MainPage.java
@@ -0,0 +1,14 @@
+package edu.hdu.personalfinancialmanagementsystems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+}
